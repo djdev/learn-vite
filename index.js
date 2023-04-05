b1918b7d08a47c7dfcff97ddd058e28ae2a57b1a
DJ <deepjyoti_rajkhowa@yahoo.com>
2023-04-04 20:16:11 -0400

    Add css module

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 // import file from "./file-1";
 // console.log(file);
 
+import styles from "./style.module.css";
+
+console.log(styles);
+document.querySelector("h1").className = styles.title;
+
 console.log("Hello World!");
